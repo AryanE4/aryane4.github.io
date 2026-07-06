59350f6441e1218e52b9ffa8ab9703ae572cc7ef
AryanE4 <aryansharmabtech.2023@gmail.com>
2026-07-06 16:09:55 +0530

    task-2: create MainMenu component with CSS modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import MainMenu from './components/MainMenu/MainMenu'
+
 function App() {
-  return <div>Loading...</div>
+  const handleSelect = (section: string) => {
+    console.log('Selected section:', section)
+  }
+
+  return <MainMenu onSelect={handleSelect} />
 }
 
 export default App
